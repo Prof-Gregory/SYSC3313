c569c30df32bc0573c56a28a83d3cf239879f475
Greg Franks <gregoryfranks@cunet.carleton.ca>
2024-11-19 11:08:57 -0500

    Add Thread Example (with history).

--- Threads/ThreadExample3.cpp
+++ Threads/ThreadExample3.cpp
@@ -1,6 +1,6 @@
 /**
  * ThreadExample.c++ - this application demonstrates how to define a
- * separate thread of execution using std::thread to run a function.
+ * separate thread of execution using std::thread to run a member function.
  *
  * Must use -std=c++11 (or better!)
  */
@@ -23,28 +23,32 @@ const static int count = 20;
  *  n! = n * (n-1)!, n > 0
  */
 
-static void factorial( const std::string& name )
-{
-    // 0! = 1
-    long factorial = 1;
-    std::cout << "0! = " << factorial << std::endl;
-    for (int n = 1; n <= count; n++) {
-	// Sleep for between 0 and 2 seconds before calculating n!
-	std::this_thread::sleep_for( std::chrono::milliseconds( static_cast<long>(f(generator) * 2000 )) );
-	// N! = n * (n-1)!
-	factorial = n * factorial;
-	std::cout << n << "! = " << factorial << std::endl;
+class Factorial {
+public:
+    void run( const std::string& name ) {
+
+	// 0! = 1
+	long factorial = 1;
+	std::cout << "0! = " << factorial << std::endl;
+	for (int n = 1; n <= count; n++) {
+	    // Sleep for between 0 and 2 seconds before calculating n!
+	    std::this_thread::sleep_for( std::chrono::milliseconds( static_cast<long>(f(generator) * 2000 )) );
+	    // n! = n * (n-1)!
+	    factorial = n * factorial;
+	    std::cout << n << "! = " << factorial << std::endl;
+	}
+	std::cout << name << "(" << std::this_thread::get_id() << ") finished" << std::endl;
     }
-    std::cout << name << "(" << std::this_thread::get_id() << ") finished" << std::endl;
-}
+};
 
 
 int main( int argc, char ** argv )
 {
     std::cerr << "Creating factorial thread." << std::endl;
-    std::thread thread( factorial, "factorial" );
+    Factorial compute;
+    std::thread thread( &Factorial::run, &compute, "Factorial::run" );	// pass object by reference.
     std::cerr << "Created: " << thread.get_id() << std::endl;
-    thread.join();		// must join or aborts
+    thread.join();
     std::cerr << "Factorial thread ended." << std::endl;
     return 0;
 }
